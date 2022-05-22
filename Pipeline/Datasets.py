4db4e781581fcbb767766892893effc80056cc51
MattS0000 <mati201420@gmail.com>
2022-05-22 16:52:11 +0200

    Add Pipeline Dir

---
+++ Pipeline/Datasets.py
@@ -0,0 +1,58 @@
+from torch.utils.data import Dataset
+import pandas as pd
+import os
+from PIL import Image
+
+path2img = './drive/MyDrive/IDRID/Images/'
+path2labels = './drive/MyDrive/IDRID/Labels/'
+
+
+class IDRiD_General_Dataset(Dataset):
+    def __init__(self, transform, data_type, csv_filename):
+        # path to images
+        path2data = os.path.join(path2img, data_type)
+        # list of images
+        filenames = os.listdir(path2data)
+        # fullpath
+        self.full_filenames = [os.path.join(path2data, f) for f in filenames]
+        # labels
+        path2csvLabels = os.path.join(path2labels, data_type, csv_filename)
+        print(path2csvLabels)
+        labels_df = pd.read_csv(path2csvLabels, index_col=[0])
+        self.labels = labels_df.iloc[:, :]
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.full_filenames)
+
+    def __getitem__(self, idx):
+        return 'fake_item_super_class', 2, 3, 4, 'fake_item_super_class'
+
+
+class IDRiD_Dataset(IDRiD_General_Dataset):
+    def __init__(self, transform, data_type="train"):
+        # path to images
+        super().__init__(transform, data_type, "labels.csv")
+
+    def __getitem__(self, idx):
+        image = Image.open(self.full_filenames[idx])
+        image = self.transform(image)
+        table = self.labels.loc[idx].to_numpy()
+        return image, table[0], table[1], table[2:4], table[4:6]
+
+
+class IDRiD_Dataset_Teacher(IDRiD_General_Dataset):
+    def __init__(self, transform, tasks, data_type="train"):
+        # path to images
+        super().__init__(transform, data_type, "M1_predictions" + str(tasks) + ".csv")
+
+    def __getitem__(self, idx):
+        image = Image.open(self.full_filenames[idx])
+        image = self.transform(image)
+        table = self.labels.loc[idx].to_numpy()
+        return image, table[0:5], table[5:10], table[10:12], table[12:]
+
+
+class IDRiD_Dataset_Unlabeled_Preds(IDRiD_Dataset_Teacher):
+    def __init__(self, transform, tasks, data_type="test"):
+        super().__init__(transform, tasks, data_type)
---
+++ Pipeline/Pipe.py
@@ -0,0 +1,100 @@
+import torch
+from torchvision import transforms
+import pandas as pd
+from torch import nn
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from Pipeline.datasets import IDRiD_Dataset, IDRiD_Dataset_teacher, IDRiD_Dataset_unlabeled_preds
+from Pipeline.models import MTL
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class Pipe():
+    def __init__(self):
+        # for all models
+        rx = 450
+        ry = 300
+        old_x = 4288
+        old_y = 2848
+        self.Rx = rx / old_x
+        self.Ry = ry / old_y
+        self.M1 = MTL('M1').to(device)
+        self.M2 = MTL('M2').to(device)
+        self.M3=MTL('M3').to(device)
+        self.data_transformer = transforms.Compose([transforms.Resize((rx, ry)), transforms.ToTensor(),
+                                                    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                                         std=[0.229, 0.224, 0.225])])
+
+    def fit_predict_M1(self, tasks, epochs):
+        self.M1_train_ds = IDRiD_Dataset(self.data_transformer, 'train')
+        self.M1_train_dl = DataLoader(self.M1_train_ds, batch_size=32, shuffle=True)
+        self.M1_optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, self.M1.parameters()),
+                                            weight_decay=1e-6,
+                                            momentum=0.9,
+                                            lr=1e-3,
+                                            nesterov=True)
+        self.M1_scheduler = ReduceLROnPlateau(self.M1_optimizer,
+                                              factor=0.5,
+                                              patience=3,
+                                              min_lr=1e-7,
+                                              verbose=True)
+        self.M1_criterion = nn.CrossEntropyLoss()
+        self.M1.fit(self.M1_train_dl, self.M1_optimizer, self.M1_scheduler, self.M1_criterion, tasks, epochs, self.Rx,
+                    self.Ry)
+        self.M1.load_state_dict(torch.load("./M1_weights" + str(tasks) + ".pt"))
+        self.M1.eval()
+        data = pd.DataFrame()
+        z = []
+        for i, (imgs, retinopathy_label, macular_edema_label, fovea_center_labels, optical_disk_labels) in enumerate(
+                self.M1_train_ds):
+            one_row = self.M1.forward(imgs[None, :].to(device))
+            z.append(torch.concat(one_row).detach().cpu().numpy())
+        data = pd.DataFrame(z)
+        data.to_csv('./drive/MyDrive/IDRID/Labels/train/M1_predictions' + str(tasks) + '.csv')
+
+    def fit_predict_M2(self, tasks, epochs):
+        self.M2_train_ds = IDRiD_Dataset_teacher(self.data_transformer, tasks, 'train')
+        self.M2_train_dl = DataLoader(self.M2_train_ds, batch_size=32, shuffle=True)
+        self.M2_optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, self.M2.parameters()),
+                                            weight_decay=1e-6,
+                                            momentum=0.9,
+                                            lr=1e-3,
+                                            nesterov=True)
+        self.M2_scheduler = ReduceLROnPlateau(self.M2_optimizer,
+                                              factor=0.5,
+                                              patience=3,
+                                              min_lr=1e-7,
+                                              verbose=True)
+        self.M2_criterion = nn.KLDivLoss(reduction='batchmean')
+        self.M2.fit(self.M2_train_dl, self.M2_optimizer, self.M2_scheduler, self.M2_criterion, tasks, epochs, self.Rx,
+                    self.Ry)
+        data = pd.DataFrame()
+        z = []
+        for i, (imgs, retinopathy_label, macular_edema_label, fovea_center_labels, optical_disk_labels) in enumerate(
+                self.M2_train_ds):
+            one_row = self.M2.forward(imgs[None, :].to(device))
+            z.append(torch.concat(one_row).detach().cpu().numpy())
+        data = pd.DataFrame(z)
+        data.to_csv('./drive/MyDrive/IDRID/Labels/train/M2_predictions' + str(tasks) + '.csv')
+
+    def fit_M3(self, tasks, epochs):
+        self.M3_train_ds = IDRiD_Dataset_unlabeled_preds(self.data_transformer, tasks, data_type='test')
+        self.M3_train_dl = DataLoader(self.M3_train_ds, batch_size=32, shuffle=True)
+        self.M3_optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, self.M3.parameters()),
+                                            weight_decay=1e-6,
+                                            momentum=0.9,
+                                            lr=1e-3,
+                                            nesterov=True)
+        self.M3_scheduler = ReduceLROnPlateau(self.M3_optimizer,
+                                              factor=0.5,
+                                              patience=3,
+                                              min_lr=1e-7,
+                                              verbose=True)
+        self.M3_criterion = nn.KLDivLoss(reduction='batchmean')
+        self.M3.fit(self.M3_train_dl, self.M3_optimizer, self.M3_scheduler, self.M3_criterion, tasks, epochs, self.Rx,
+                    self.Ry)
+
+    def fit_pipe(self, tasks, epochs):
+        self.fit_predict_M1(tasks, epochs)
+        self.fit_predict_M2(tasks, epochs)
+        self.fit_M3(tasks, epochs)
